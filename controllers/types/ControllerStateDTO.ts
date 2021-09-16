680e3861fa41fff196a85f05a673e40d57cf438f
Jaakko Heusala <jheusala@iki.fi>
2021-09-17 00:18:29 +0300

    Improved ControllerStateDTO type checks

--- controllers/types/ControllerStateDTO.ts
+++ controllers/types/ControllerStateDTO.ts
@@ -1,10 +1,10 @@
 // Copyright (c) 2021. Sendanor <info@sendanor.fi>. All rights reserved.
 
-import ControllerType from "./ControllerType";
-import ControllerState from "./ControllerState";
-import { isRegularObject } from "../../../ts/modules/lodash";
+import ControllerType, { isControllerType } from "./ControllerType";
+import ControllerState, { isControllerState } from "./ControllerState";
+import { isRegularObject, isString } from "../../../ts/modules/lodash";
 
-export interface ControllerControllerStateDTO {
+export interface ControllerStateDTO {
 
     readonly type  : ControllerType;
     readonly state : ControllerState;
@@ -12,19 +12,17 @@ export interface ControllerControllerStateDTO {
 
 }
 
-export function isControllerStateDTO (value: any): value is ControllerControllerStateDTO {
+export function isControllerStateDTO (value: any): value is ControllerStateDTO {
     return (
         isRegularObject(value)
-        // && hasNoOtherKeys(value, [
-        //     'type',
-        //     'state',
-        //     'name'
-        // ])
+        && isControllerType(value?.type)
+        && isControllerState(value?.state)
+        && isString(value?.name)
     );
 }
 
-export function stringifyControllerStateDTO (value: ControllerControllerStateDTO): string {
+export function stringifyControllerStateDTO (value: ControllerStateDTO): string {
     return `ControllerStateDTO(${value})`;
 }
 
-export default ControllerControllerStateDTO;
+export default ControllerStateDTO;
